3ff01e07c319f262df50e8512440840169343088
crasheng <eng.ahmeda91@gmail.com>
2025-07-08 17:06:05 +0300

    update the naming

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -23,9 +23,6 @@ class ServiceProvider extends AddonServiceProvider
         // Load views
         $this->loadViewsFrom(__DIR__.'/../resources/views', 'complex-collection-ordering');
 
-        // Merge config
-        $this->mergeConfigFrom(__DIR__.'/../../config/complex-collection.php', 'complex-collection');
-
         // Publish config
         if ($this->app->runningInConsole()) {
             $this->publishes([
@@ -33,16 +30,21 @@ class ServiceProvider extends AddonServiceProvider
             ], 'config');
         }
 
-        // Publish migrations
-        $this->publishes([
-            __DIR__.'/../database/migrations' => database_path('migrations'),
-        ], 'complex-collection-ordering-migrations');
+        // Only try to merge config if the file exists
+        $configPath = config_path('complex-collection.php');
+        if (file_exists($configPath)) {
+            $this->mergeConfigFrom($configPath, 'complex-collection');
+        } else {
+            // Fallback to default config if published config doesn't exist
+            $this->mergeConfigFrom(__DIR__.'/../../config/complex-collection.php', 'complex-collection');
+        }
+
     }
 
     protected function registerNavigation()
     {
         \Statamic\Facades\CP\Nav::extend(function ($nav) {
-            $nav->create('Complex Collection Ordering')
+            $nav->create(config('complex-collection.navigation_title', 'Complex Collection Ordering'))
                 ->section('Tools')
                 ->route('complex-collection-ordering.index')
                 ->icon('entries');
