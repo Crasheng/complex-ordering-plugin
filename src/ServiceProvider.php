c9df03815aa3974c157be1bb1e618cd8488c5aec
crasheng <eng.ahmeda91@gmail.com>
2025-07-09 17:19:08 +0300

    fix config publishing

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -3,7 +3,6 @@
 namespace Osmanco\ComplexCollection;
 
 use Statamic\Providers\AddonServiceProvider;
-use Osmanco\ComplexCollection\Http\Controllers\ItemController;
 
 class ServiceProvider extends AddonServiceProvider
 {
@@ -24,21 +23,12 @@ class ServiceProvider extends AddonServiceProvider
         $this->loadViewsFrom(__DIR__.'/../resources/views', 'complex-collection-ordering');
 
         // Publish config
-        if ($this->app->runningInConsole()) {
-            $this->publishes([
-                __DIR__.'/../config/complex-collection.php' => config_path('complex-collection.php'),
-            ], 'config');
-        }
+        $this->publishes([
+            __DIR__.'/../config/complex-collection.php' => config_path('complex-collection.php'),
+        ], 'config');
 
-        // Only try to merge config if the file exists
-        $configPath = config_path('complex-collection.php');
-        if (file_exists($configPath)) {
-            $this->mergeConfigFrom($configPath, 'complex-collection');
-        } else {
-            // Fallback to default config if published config doesn't exist
-            $this->mergeConfigFrom(__DIR__.'/../../config/complex-collection.php', 'complex-collection');
-        }
-
+        // Merge config (use internal default if no published file yet)
+        $this->mergeConfigFrom(__DIR__.'/../config/complex-collection.php', 'complex-collection');
     }
 
     protected function registerNavigation()
@@ -51,3 +41,62 @@ class ServiceProvider extends AddonServiceProvider
         });
     }
 }
+
+//
+//namespace Osmanco\ComplexCollection;
+//
+//use Statamic\Providers\AddonServiceProvider;
+//use Osmanco\ComplexCollection\Http\Controllers\ItemController;
+//
+//class ServiceProvider extends AddonServiceProvider
+//{
+//    protected $routes = [
+//        'cp' => __DIR__.'/../routes/cp.php',
+//    ];
+//
+//    protected $namespace = 'Osmanco\\ComplexCollection\\Http\\Controllers';
+//
+//    public function boot()
+//    {
+//        parent::boot();
+//
+//        // Register the navigation item
+//        $this->registerNavigation();
+//
+//        // Load views
+//        $this->loadViewsFrom(__DIR__.'/../resources/views', 'complex-collection-ordering');
+//
+//        // Publish config
+//        $this->publishes([
+//            __DIR__.'/../config/complex-collection.php' => config_path('complex-collection.php'),
+//        ], 'config');
+//        
+//        $configPath = config_path('complex-collection.php');
+//        if (file_exists($configPath)) {
+//            $this->mergeConfigFrom($configPath, 'complex-collection');
+//        } else {
+//            $this->mergeConfigFrom(__DIR__.'/../config/complex-collection.php', 'complex-collection');
+//        }
+//        
+//
+//        // Only try to merge config if the file exists
+//        $configPath = config_path('complex-collection.php');
+//        if (file_exists($configPath)) {
+//            $this->mergeConfigFrom($configPath, 'complex-collection');
+//        } else {
+//            // Fallback to default config if published config doesn't exist
+//            $this->mergeConfigFrom(__DIR__.'/../config/complex-collection.php', 'complex-collection');
+//        }
+//
+//    }
+//
+//    protected function registerNavigation()
+//    {
+//        \Statamic\Facades\CP\Nav::extend(function ($nav) {
+//            $nav->create(config('complex-collection.navigation_title', 'Complex Collection Ordering'))
+//                ->section('Tools')
+//                ->route('complex-collection-ordering.index')
+//                ->icon('entries');
+//        });
+//    }
+//}
